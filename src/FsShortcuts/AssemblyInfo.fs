debe533c2dfd254f2e039495ad6cb16ecd9d9f2d
Ling Yan <neil.yan@outlook.com>
2017-12-17 18:22:41 -0800

    Change names

--- src/FsShortcuts/AssemblyInfo.fs
+++ src/FsShortcuts/AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("FsWork")>]
+[<assembly: AssemblyTitle("FsShortcuts")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("FsWork")>]
+[<assembly: AssemblyProduct("FsShortcuts")>]
 [<assembly: AssemblyCopyright("Copyright ©  2017")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
